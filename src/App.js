e28472a3e6abc9ba580cf36a5d8f1ec363f2d5ca
peter-wairimu <peter.wairimu@student.moringaschool.com>
2022-11-30 15:37:28 +0300

    intial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Home from './components/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Navbar />
       </header>
+
+      <div className='app-home'>
+        <Home />     
+
+      </div>
+      <div className='footer'>
+        <Footer />
+
+      </div>
     </div>
   );
 }
